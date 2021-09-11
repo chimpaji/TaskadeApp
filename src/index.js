b7f80daa2c0fb855f16b0b0cfc80584e473e2757
chimpaji <chimpaji@outlook.com>
2021-09-11 23:08:22 +0700

    connect to mongodb

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
-const { ApolloServer, gql } = require("apollo-server");
+require("dotenv").config();
 
+const { MongoClient } = require("mongodb");
+const { ApolloServer, gql } = require("apollo-server");
+const { DB_URI, DB_NAME } = process.env;
+
+async function listDatabases(client) {
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+}
+
+const start = async () => {
+  const client = new MongoClient(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  await client.connect();
+  const db = client.db(DB_NAME);
+  await listDatabases(client);
+
+  const context = { db };
+
+  const server = new ApolloServer({ typeDefs, resolvers, context });
+
+  // The `listen` method launches a web server.
+  server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
+};
+
+start().catch(console.error);
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
@@ -41,9 +72,3 @@ const resolvers = {
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
-
-// The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});

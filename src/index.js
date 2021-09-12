6694d2293628a5d46f4c02bea237f573bab32ec1
chimpaji <chimpaji@outlook.com>
2021-09-12 13:01:27 +0700

    auth signin signup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
-
 const { MongoClient } = require("mongodb");
 const { ApolloServer, gql } = require("apollo-server");
+const bcrypt = require("bcrypt");
 const { DB_URI, DB_NAME } = process.env;
+const saltRounds = 10;
 
 async function listDatabases(client) {
   databasesList = await client.db().admin().listDatabases();
@@ -61,10 +62,85 @@ const typeDefs = gql`
     isCompleted: Boolean!
     taskList: TaskList!
   }
+
+  type Mutation {
+    signUp(input: SignupInput): AuthUser
+    signIn(input: SigninInput): AuthUser
+  }
+
+  input SigninInput {
+    email: String!
+    password: String!
+  }
+
+  input SignupInput {
+    email: String!
+    password: String!
+    name: String!
+    avatar: String
+  }
+
+  type AuthUser {
+    user: User!
+    token: String!
+  }
 `;
 
 const resolvers = {
+  User: {
+    id: (root, data, context) => {
+      //root will show what mutation return in User
+      console.log("root", root);
+      return root._id;
+    },
+  },
   Query: {},
+  Mutation: {
+    signUp: async (root, data, context) => {
+      const { db } = context;
+      const { input } = data;
+      const existUser = await db
+        .collection("Users")
+        .findOne({ email: input.email });
+      if (existUser) throw new Error(`Email ${input.email} already exists`);
+      const hashedPassword = bcrypt.hashSync(input.password, saltRounds);
+      const newUser = { ...input, password: hashedPassword };
+      let id;
+      const result = await db
+        .collection("Users")
+        .insertOne(newUser, function (error, response) {
+          if (error) {
+            console.log("Error occurred while inserting");
+            // return
+          } else {
+            console.log("inserted record", response);
+            id = response.insertedIds;
+            return response;
+          }
+        });
+      const user = await db.collection("Users").findOne({ email: input.email });
+      console.log("resutl=>", result);
+      console.log("user=>", user);
+      return { user: user, token: "token123" };
+    },
+    signIn: async (root, data, context) => {
+      const { input } = data;
+      const { db } = context;
+
+      const existUser = await db
+        .collection("Users")
+        .findOne({ email: input.email });
+      if (!existUser) throw new Error("Invalid Credentials");
+
+      const validPassword = bcrypt.compareSync(
+        input.password,
+        existUser.password
+      );
+      if (!validPassword) throw new Error("Invalid Credentials");
+
+      return { token: "token123", user: existUser };
+    },
+  },
 };
 
 // The ApolloServer constructor requires two parameters: your schema
